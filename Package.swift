c26934908f0b28b6a9691e6c7316bddb13b8199b
lquintero07 <37584610+lquintero07@users.noreply.github.com>
2026-03-01 22:58:50 -0500

    Update Package.swift
    Signed-off-by: lquintero07 <37584610+lquintero07@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "GoogleMLKitSwiftPM",
-  platforms: [.iOS(.v15)],
+  platforms: [.iOS(.v15), .macOS(.v10_15)],
   products: [
     .library(
       name: "MLKitBarcodeScanning",
